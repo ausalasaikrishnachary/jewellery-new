7a1213ea3d4d25240d3118835db6fd4f1dbab6cb
bharathkusuma123 <bharathkusuma4953@gmail.com>
2024-11-23 11:46:08 +0530

    bharath

--- src/Pages/Brands/Jewels.js
+++ src/Pages/Brands/Jewels.js
@@ -3,8 +3,11 @@ import React from 'react'
 function Jewels() {
   return (
     <div>
-      Jewels
-    </div>
+          <h3 >Sadashri Jewels</h3>
+          <p >
+            Sadashri Jewels is an <strong className='strongoffline'>offline</strong> jewelry store known for its timeless designs and premium craftsmanship. We bring you an exclusive collection that blends tradition with elegance.
+          </p>
+          </div>
   )
 }
 
