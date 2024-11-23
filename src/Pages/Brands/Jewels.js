6236dc1c7f436c066b21cd2eb2cd7509a86e7735
sai <asaikrishnachary103@gmail.com>
2024-11-23 11:54:54 +0530

    sai

--- src/Pages/Brands/Jewels.js
+++ src/Pages/Brands/Jewels.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 function Jewels() {
-  return (
-    <div>
-          <h3 >Sadashri Jewels</h3>
-          <p >
-            Sadashri Jewels is an <strong className='strongoffline'>offline</strong> jewelry store known for its timeless designs and premium craftsmanship. We bring you an exclusive collection that blends tradition with elegance.
-          </p>
-          </div>
-  )
+    return (
+        <div style={{paddingTop:"100px"}}>
+            
+                <h3 >Sadashri Jewels</h3>
+                <p >
+                    Sadashri Jewels is an <strong className='strongoffline'>offline</strong> jewelry store known for its timeless designs and premium craftsmanship. We bring you an exclusive collection that blends tradition with elegance.
+                </p>
+           
+        </div>
+    )
 }
 
 export default Jewels
